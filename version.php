b8402fd6bca391613b46f702a081f10ab075659f
valery <valery.fremaux@gmail.com>
2016-09-28 01:32:59 +0200

    automated sync from SVN

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2015111500;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2014050800;        // Requires this Moodle version
+$plugin->requires  = 2014110400;        // Requires this Moodle version
 $plugin->component = 'filter_cms'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.7.0 (Build 201511500)';
+$plugin->release = '2.8.0 (Build 201511500)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '2.7.0000';
+$plugin->codeincrement = '2.8.0000';
